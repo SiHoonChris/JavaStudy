7f6bcec3492bb8815c21efb372e8c59be1c853e8
SiHoonChris <chrisleegpock@gmail.com>
2022-10-08 16:32:51 +0900

    10-08-2022

--- JVs/src/SelfMade/SquidGame5thGameFin.java
+++ JVs/src/SelfMade/SquidGame5thGameFin.java
@@ -8,7 +8,7 @@ public class SquidGame5thGameFin
 		int[][] Bridge = new int[18][2];
 		Scanner yn = new Scanner(System.in);
 		Scanner choice = new Scanner(System.in);
-		int num=1;
+		int num=0;
 		int go=0;
 		int YesNo = 0;
 		
@@ -20,8 +20,8 @@ public class SquidGame5thGameFin
 				else 	 Bridge[i][1]=1;
 			}
 		}
-//		(정답)
-//		System.out.println("<  정답  >\n ");
+//		(정답 미리보기)
+//		System.out.println("<ANSWER>");
 //		for(int i=0; i<Bridge.length; i++) {
 //			for(int j=0; j<Bridge[0].length; j++) {
 //				System.out.print(Bridge[i][j]);
@@ -57,13 +57,13 @@ public class SquidGame5thGameFin
 		System.out.println("Then, Good Luck.\n");
 		System.out.println("< ↓ Start Here ↓ >\n");
 		System.out.println("     L  R");
-		for(int i=1; i<=18; i++) System.out.println("     □  □ ["+i+"]");
+		for(int i=1; i<=18; i++) System.out.println("     ■  ■ ["+i+"]");
 		System.out.println("\n< -- End Here -- >");
 		System.out.println("\nRight of Left");
 		
 		// (진행) 만약 주어진 행에서 선택한 열이 1(깨지는 유리)이면 게임 종료, 0(안깨지는 유리)이면 계속
 		while(true) {
-			System.out.println("["+num+"/18]");
+			System.out.println("["+(num+1)+"/18]");
 			System.out.print("=> ");
 			String Choice = choice.next();
 			if(Choice.equals("L")||Choice.equals("l")) go=0;
@@ -76,20 +76,25 @@ public class SquidGame5thGameFin
 			}
 			if(Bridge[num][go]==0) num++;{				
 				if(num==18) {
-					System.out.println("Congratulations.");
+					System.out.println("\nCongratulations.");
 					break;	
 				}
 			} 
 		}
 		
 		// 정답 공개
-		System.out.println("\n----------------\n<  정답  >\n ");
+		System.out.println("\n----------------\n <ANSWER>\n ");
+		System.out.println("  L   R");
 		for(int i=0; i<Bridge.length; i++) {
 			for(int j=0; j<Bridge[0].length; j++) {
-				System.out.print(Bridge[i][j]);
+				if(Bridge[i][j]==0) Bridge[i][j]='■';
+				else				Bridge[i][j]='X';
+				System.out.print("  "+(char)Bridge[i][j]+" ");
 			}
-			System.out.printf("   [%d]",i+1);
+			System.out.printf(" [%d]",i+1);
 			System.out.println();
 		}
+		yn.close();
+		choice.close();
 	}
 }
