c47a6ddd95dca1c3ca1fa93bcc143cd4fc33dc4a
SiHoonChris <chrisleegpock@gmail.com>
2022-10-12 18:25:06 +0900

    10-12-2022

--- JVs/src/SelfMade/CellPhone.java
+++ JVs/src/SelfMade/CellPhone.java
@@ -1,66 +1,67 @@
 package SelfMade;
 import java.util.Scanner;
 import java.util.Arrays;
-//----------------------- class PhoneBook ------------------------//
+//-----------------------------------------------------//
+//                   class PhoneBook
+//-----------------------------------------------------//
 class PhoneBook{
-	String[][] Book = new String[3][2];      // 연락처(이름, 전화번호) 
-	String name;                             // 이름
-	String number;                           // 전화번호
-	boolean call;                            // 발신
-	boolean receive;                         // 수신
-    
+	// 연락처 [이름, 전화번호]
+	static String[][] Book = new String[3][2];
+	String name;                    // 이름
+	String number;                  // 전화번호
+	boolean call;                   // 발신
+	boolean receive;                // 수신
+	boolean operate=true;           // 프로그램 작동
+	boolean full;                   // 잔여 저장 공간 0
+	
 	// 연락처 저장
-	void save(String name, String number) {
-		try {			
+	void save() {
+		int cnt=0;
+		while(operate) {
+			if(cnt==Book.length) {
+				System.out.println("저장 공간 부족");
+				full=true;
+				break;
+			}
+			if(full) operate = !operate;
+			
 			Scanner sc = new Scanner(System.in);
-			System.out.print("이름 : ");
+			System.out.print("이름 :  ");
 			name = sc.next();
-			System.out.print("연락처('-'포함) : ");
+			System.out.print("전화번호 :  ");
 			number = sc.next();
-			
-			Exception e = new Exception("연락처 재확인 바랍니다(저장 실패)");
-			if(number.length()!=13) throw e;
-			
-			int cnt=0;
-			for(int i=0; i<Book.length; i++) {
-				cnt++;
-				if(cnt>Book.length) {						
-					System.out.println("저장 공간 부족(저장 실패)");					
+		
+			for(int i=0 ; i<Book.length ; i++) {
+				if(Book[i][0]==null&&Book[i][1]==null) {					
+					Book[i][0]=name;
+					Book[i][1]=number;
+					System.out.printf("저장 성공(%d/%d)\n", cnt, Book.length);
+					cnt++;
 					break;
-				} else {
-					if(Book[i][0]==null && Book[i][1]==null) {
-						Book[i][0]=name;
-						Book[i][1]=number;
-						System.out.println("저장이 완료되었습니다.");
-						System.out.printf("남은 공간 : %d/%d\n", Book.length-cnt, Book.length);					
-						break;
-					}
 				}
-			} 
-		} catch(Exception e) { System.out.println(e.getMessage()); }
+			}
+	
+			System.out.println("계속?(1.계속, 2.종료)");
+			System.out.print("=>  ");
+			int answer = sc.nextInt();		
+			if(answer==2) operate = !operate;
+		}	
 	}
+	
+	
 	// 이름으로 연락처 검색(Scanner)
 	void search() {}
-	void delete() {}                            // 연락처 삭제
-	void call()    { call = !call; }            // 전화 걸기
-	void receive() { receive = !receive; }      // 전화 받기
+	void delete() {}                          // 연락처 삭제
+	void call()    { call = !call; }          // 전화 걸기
+	void receive() { receive = !receive; }    // 전화 받기
 }
-//----------------------- class PhoneBook ------------------------//
 
-//-------------------- public class CellPhone --------------------//
+//-----------------------------------------------------//
+//               public class CellPhone
+//-----------------------------------------------------//
 public class CellPhone {
 	public static void main(String[] args) {
-		PhoneBook p = new PhoneBook();
-		p.save("name", "number");
-		p.save("name", "number");
-		p.save("name", "number");
-		p.save("name", "number");
-		p.save("name", "number");
-		
-		System.out.println(Arrays.toString(p.Book[0]));
-		System.out.println(Arrays.toString(p.Book[1]));
-		System.out.println(Arrays.toString(p.Book[2]));
-		System.out.println(Arrays.toString(p.Book[3]));
+		PhoneBook pb = new PhoneBook();
+		pb.save();
 	}
 }
-//-------------------- public class CellPhone --------------------//
