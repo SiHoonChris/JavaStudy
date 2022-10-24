3883b58b9d55c4bfbe46e8adfdbda3fc4dd9be2b
SiHoonChris <chrisleegpock@gmail.com>
2022-10-24 23:38:29 +0900

    10-24-2022

--- JVs/src/SelfMade/BlackjackADV.java
+++ JVs/src/SelfMade/BlackjackADV.java
@@ -9,8 +9,8 @@ import java.util.Arrays;
 // ---------- Card ---------- //
 // Only One Constructor, No Methods
 class Card{
-	String pattern=""; // SPADE, CLOVER, DIAMOND, HEART (4)
-	String number="";  // A, 2~10, J, Q, K             (13)
+	String pattern="";
+	String number="";
 	
 	Card(int x, int y){
 		switch(x){
@@ -67,6 +67,8 @@ class onTheTable{
 	int choice=0;
 	int score;
 	
+	inTheGame G = new inTheGame();
+	
 	// 0. 카드 재생성
 	private void converter() {
 		for(int i=0; i<cd.Deck.length; i++) {
@@ -108,13 +110,14 @@ class onTheTable{
 		System.out.print(cd.Deck[2].pattern+"-"+cd.Deck[2].number+"\n");
 		if(cd.Deck[0].number=="A"||cd.Deck[2].number=="A") { whatIsYourAce(); }
 		
-		// HIT or STAY 결정
 		hitOrStay();
 
 		System.out.printf("=> [%02d]", Arrays.stream(Player).sum());
 		System.out.println();
 		System.out.println("===============================================");
+		
 		whoWins();
+		G.cardsLeftInTheDeck(Dealer, Player);
 	}
 	// 2. A값 선택
 	private void whatIsYourAce() {
@@ -131,24 +134,23 @@ class onTheTable{
 		if(Answer==11) {Player[n]=11;}
 		else 		   { return;}
 	}
-	
-	// 3. 플레이어 HIT/STAY  *게이머(플레이어) : 얼마든지 카드를 추가로 뽑을 수 있다.(HIT or STAY)
+	// 3. 플레이어
 	private void hitOrStay() {
-		if(Dealer[2]==0) {  // Dealer가 세 번째 카드 받지 않음
+		if(Dealer[2]==0) {
 			int num=2;
 			for(int i=4 ; ; i++) {
 				System.out.print("HIT or STAY ? =>  ");
 				String select = sc.next();
-					if(select.equals("HIT")) { // HIT 입력 시, 카드 부여				
+					if(select.equals("HIT")) {		
 						Player[num]=gamingDeck[i];
 						System.out.print(cd.Deck[i].pattern+"-"+cd.Deck[i].number+"\n");
 						if(cd.Deck[i].number=="A") { whatIsYourAce(num); }
 						num++;
-					} else if(select.equals("STAY")) { return; } // STAY 입력 시, 카드 부여 안하고 반복문 종료
-				if(Arrays.stream(Player).sum()>21) { return; }  // Player가 받은 카드의 합이 21을 넘으면 bust, 자동으로 게임 종료
+					} else if(select.equals("STAY")) { return; }
+				if(Arrays.stream(Player).sum()>21) { return; }
 			}
 		}
-		else {  // Dealer가 세 번째 카드 받지 않음
+		else {
 			int num=2;
 			for(int i=5 ; ; i++) {
 				System.out.print("HIT or STAY ? =>  ");
@@ -182,13 +184,27 @@ class onTheTable{
 } // end - class onTheTable
 
 //---------- game ---------- //
-// Methods : 
+// Methods : cardsLeftInTheDeck(), infoOfMatch()
 class inTheGame{
-	public void cardsLeftInTheDeck() {}
-	// To do...
-	// 카드가 52장 다 소모될 때까지 반복하기
-	// (굳이 배열 안건들이고, 변수 52로 초기화 해놓고 카드 주어질 때마다 카운해서 빼면 될듯)
-	// 게임 종료 멘트 & 게임 중 현황 표시(몇 번째 턴인지, 현재 전적, 남은 카드 장 수)	
+	int cardAtFirst = cardDeck.Deck.length;
+	
+	// 0. 남은 카드 장 수 계산
+	public void cardsLeftInTheDeck(int[] Dealer, int[] Player) {
+		int dealerRemain=0;
+		int playerRemain=0;
+		// 딜러가 소모한 카드 장 수 계산
+		for(int i=0; i<Dealer.length; i++) { if(Dealer[i]!=0) dealerRemain++; }
+		// 플레이어가 소모한 카드 장 수 계산
+		for(int i=0; i<Player.length; i++) { if(Player[i]!=0) playerRemain++; }
+		
+		int cardRemain = cardAtFirst - (dealerRemain+playerRemain);
+		System.out.println("남은 카드 : "+cardRemain);
+	}
+	
+	public void infoOfMatch() {
+		// 게임 종료 멘트 & 게임 중 현황 표시(몇 번째 턴인지, 현재 전적, 남은 카드 장 수)	
+		System.out.println(cardDeck.Deck.length);
+	}
 } // end - public class gameMustGoOn
 
 // ---------- Play Here ---------- // Main Class
@@ -197,12 +213,12 @@ public class BlackjackADV{
 	public static void main(String[] args){
 		cardDeck cd = new cardDeck();
 		onTheTable T = new onTheTable();
+		inTheGame G = new inTheGame();
 		cd.cardsInTheBox();
 		cd.shuffle();	
 	
 		System.out.println("===============================================");
 		T.getYourCards();
-		
 //		System.out.println(Arrays.toString(T.Dealer));  // 배열(받은 카드 내역) 확인
 //		System.out.println(Arrays.toString(T.Player));  // 배열(받은 카드 내역) 확인
 		
